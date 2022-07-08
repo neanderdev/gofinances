3a0d0675799cad38dbf1576099adfa7e90b0a6d6
neanderdev <neander.faria@hotmail.com>
2022-07-08 14:38:42 -0300

    Getting to know Styled Components

--- App.tsx
+++ App.tsx
@@ -1,21 +1,7 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
 
-import { Welcome } from './src/components/Welcome';
+import { Dashboard } from './src/screens/Dashboard';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Welcome title='React Native Bare Workflow com Typescript' />
-    </View>
-  );
+  return <Dashboard />
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
