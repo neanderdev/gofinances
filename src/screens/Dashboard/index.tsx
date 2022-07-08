b4d3a3521df0ff95f80f80ab597718889a6ebe5d
neanderdev <neander.faria@hotmail.com>
2022-07-08 16:21:31 -0300

    Using proportions in measurements

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,33 @@
 import React from 'react';
 
-import { Container, Title } from './styles';
+import {
+    Container,
+    Header,
+    UserWrapper,
+    UserInfo,
+    Photo,
+    User,
+    UserGreenting,
+    UserName
+} from './styles';
 
 export function Dashboard() {
     return (
         <Container>
-            <Title>Dashboard</Title>
+            <Header>
+                <UserWrapper>
+                    <UserInfo>
+                        <Photo
+                            source={{ uri: "https://github.com/neanderdev.png" }}
+                        />
+
+                        <User>
+                            <UserGreenting>Olá,</UserGreenting>
+                            <UserName>Neander</UserName>
+                        </User>
+                    </UserInfo>
+                </UserWrapper>
+            </Header>
         </Container>
     );
 }
