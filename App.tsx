bd255116bcc40074b857dfb08c8cc40cb158f2a9
neanderdev <neander.faria@hotmail.com>
2022-07-08 15:51:52 -0300

    Using Custom Fonts

--- App.tsx
+++ App.tsx
@@ -1,14 +1,61 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+import { View } from 'react-native';
 import { ThemeProvider } from 'styled-components';
+import * as SplashScreen from 'expo-splash-screen';
+import * as Font from 'expo-font';
+import {
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold
+} from '@expo-google-fonts/poppins';
 
 import theme from "./src/global/styles/theme";
 
 import { Dashboard } from './src/screens/Dashboard';
 
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+
+        await Font.loadAsync({
+          Poppins_400Regular,
+          Poppins_500Medium,
+          Poppins_700Bold
+        });
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, [])
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
   return (
     <ThemeProvider theme={theme}>
-      <Dashboard />
+      <View
+        onLayout={onLayoutRootView}
+        style={{
+          flex: 1
+        }}
+      >
+        <Dashboard />
+      </View>
     </ThemeProvider>
   )
 }
