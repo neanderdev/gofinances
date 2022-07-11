85a557177b0dee335235629454c95a71a738ed51
neanderdev <neander.faria@hotmail.com>
2022-07-11 10:41:45 -0300

    Creating the Component to select the transaction type

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import {
     Container,
@@ -6,12 +6,20 @@ import {
     Title,
     Form,
     Fields,
+    TransactionsTypes,
 } from "./styles";
 
 import { Input } from "../../components/Form/Input";
 import { Button } from "../../components/Form/Button";
+import { TransactionTypeButton } from "../../components/Form/TransactionTypeButton";
 
 export function Register() {
+    const [transactionType, setTransactionType] = useState('');
+
+    function handleTransactionsTypeSelect(type: 'up' | 'down') {
+        setTransactionType(type);
+    }
+
     return (
         <Container>
             <Header>
@@ -27,6 +35,22 @@ export function Register() {
                     <Input
                         placeholder="Preço"
                     />
+
+                    <TransactionsTypes>
+                        <TransactionTypeButton
+                            type='up'
+                            title="Income"
+                            onPress={() => handleTransactionsTypeSelect('up')}
+                            isActive={transactionType === 'up'}
+                        />
+
+                        <TransactionTypeButton
+                            type='down'
+                            title="Outcome"
+                            onPress={() => handleTransactionsTypeSelect('down')}
+                            isActive={transactionType === 'down'}
+                        />
+                    </TransactionsTypes>
                 </Fields>
 
                 <Button
