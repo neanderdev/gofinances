f4092f6832cab32f24e86d63f6aa81f255657c6d
neanderdev <neander.faria@hotmail.com>
2022-07-11 17:29:58 -0300

    Typing the button

--- src/components/Form/Button/styles.ts
+++ src/components/Form/Button/styles.ts
@@ -1,8 +1,13 @@
 import styled from "styled-components/native";
-import { RectButton } from "react-native-gesture-handler";
+import { RectButton, RectButtonProps } from "react-native-gesture-handler";
 import { RFValue } from "react-native-responsive-fontsize";
+import { PropsWithChildren } from "react";
 
-export const Container = styled(RectButton)`
+interface ButtonProps extends PropsWithChildren<RectButtonProps> {
+  // caso queira adicionar props customizadas, adicione aqui.
+}
+
+export const Container = styled(RectButton)<ButtonProps>`
   width: 100%;
   background-color: ${({ theme }) => theme.colors.secondary};
 
