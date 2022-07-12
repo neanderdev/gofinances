b0a7e5183b43ad6a0d6eb653595309fcc32e9d97
neanderdev <neander.faria@hotmail.com>
2022-07-12 14:25:34 -0300

    Finalizing the filter

--- src/screens/Resume/index.tsx
+++ src/screens/Resume/index.tsx
@@ -4,6 +4,8 @@ import { VictoryPie } from "victory-native";
 import { RFValue } from "react-native-responsive-fontsize";
 import { useTheme } from "styled-components";
 import { useBottomTabBarHeight } from "@react-navigation/bottom-tabs";
+import { addMonths, subMonths, format } from "date-fns";
+import { ptBR } from "date-fns/locale";
 
 import {
     Container,
@@ -39,9 +41,18 @@ interface CategoryData {
 }
 
 export function Resume() {
+    const [selectedDate, setSelectedDate] = useState(new Date());
     const [totalByCategories, setTotalByCategories] = useState<CategoryData[]>([]);
 
     const theme = useTheme();
+
+    function handleDateChange(action: "next" | "prev") {
+        if (action === "next") {
+            setSelectedDate(addMonths(selectedDate, 1));
+        } else {
+            setSelectedDate(subMonths(selectedDate, 1));
+        }
+    }
 
     async function loadData() {
         const dataKey = '@gofinances:transactions';
@@ -49,7 +60,11 @@ export function Resume() {
         const response = await AsyncStorage.getItem(dataKey);
         const responseFormatted = response ? JSON.parse(response) : [];
 
-        const expensives = responseFormatted.filter((expensive: TransactionData) => expensive.type === 'negative');
+        const expensives = responseFormatted.filter((expensive: TransactionData) =>
+            expensive.type === 'negative' &&
+            new Date(expensive.date).getMonth() === selectedDate.getMonth() &&
+            new Date(expensive.date).getFullYear() === selectedDate.getFullYear()
+        );
 
         const expensivesTotal = expensives.reduce((acumullator: number, expensive: TransactionData) => {
             return acumullator + Number(expensive.amount);
@@ -90,7 +105,7 @@ export function Resume() {
 
     useEffect(() => {
         loadData();
-    }, [])
+    }, [selectedDate])
 
     return (
         <Container>
@@ -106,13 +121,15 @@ export function Resume() {
                 }}
             >
                 <MonthSelect>
-                    <MonthSelectButton>
+                    <MonthSelectButton onPress={() => handleDateChange('prev')}>
                         <MonthSelectIcon name="chevron-left" />
                     </MonthSelectButton>
 
-                    <Month>Julho de 2022</Month>
+                    <Month>
+                        {format(selectedDate, 'MMMM, yyyy', { locale: ptBR })}
+                    </Month>
 
-                    <MonthSelectButton>
+                    <MonthSelectButton onPress={() => handleDateChange('next')}>
                         <MonthSelectIcon name="chevron-right" />
                     </MonthSelectButton>
                 </MonthSelect>
