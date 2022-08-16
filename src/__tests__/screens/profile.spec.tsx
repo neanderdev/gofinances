be6873e4ff50086d8b97cbeaa0dd0c8707543ac6
neanderdev <neander.faria@hotmail.com>
2022-08-16 16:17:16 -0300

    First Test

--- src/__tests__/screens/profile.spec.tsx
+++ src/__tests__/screens/profile.spec.tsx
@@ -1,10 +1,12 @@
-import React from "react";
-import { render } from "@testing-library/react-native";
+import React from 'react'
+import { render } from '@testing-library/react-native'
 
-import { Profile } from "../../screens/Profile";
+import { Profile } from '../../screens/Profile'
 
 test('check if show correctly user input name placeholder', () => {
-    const { debug } = render(<Profile />);
+    const { getByPlaceholderText } = render(<Profile />);
 
-    debug();
-});
+    const inputName = getByPlaceholderText('Nome');
+
+    expect(inputName.props.placeholder).toBeTruthy();
+})
