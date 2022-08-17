3e57b2d3cfc28bf4f1de2db43c43901d255e6b7e
neanderdev <neander.faria@hotmail.com>
2022-08-17 10:28:30 -0300

    Test on Components

--- src/components/Form/Input/index.tsx
+++ src/components/Form/Input/index.tsx
@@ -3,10 +3,12 @@ import { TextInputProps } from "react-native";
 
 import { Container } from "./styles";
 
-type InputProps = TextInputProps;
+interface InputProps extends TextInputProps {
+    active?: boolean;
+}
 
-export function Input({ ...rest }: InputProps) {
+export function Input({ active = false, ...rest }: InputProps) {
     return (
-        <Container {...rest} />
+        <Container active={active} {...rest} />
     );
 }
