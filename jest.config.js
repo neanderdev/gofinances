daff0facea2ef16e110375c295cffff37fc2a6a3
neanderdev <neander.faria@hotmail.com>
2022-08-17 15:27:14 -0300

    Code Coverage

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,23 @@
 module.exports = {
   preset: "jest-expo",
+
   testPathIgnorePatterns: ["/node-modules", "/android", "/ios"],
+
   transform: {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
       "<rootDir>/fileTransformer.js",
   },
+
   setupFilesAfterEnv: [
     "@testing-library/jest-native/extend-expect",
     "jest-styled-components",
   ],
+
   setupFiles: ["./path/to/jestSetupFile.js"],
+
+  collectCoverage: true,
+
+  collectCoverageFrom: ["src/**/*.tsx", "!src/**/*.spec.tsx"],
+
+  coverageReporters: ["lcov"],
 };
